b307dcad00337a6c1ae940c9dfb35ac28b706b7d
henanewind <zzwonder2009@126.com>
2021-04-12 16:36:58 +0800

    + 增加浏览器动画渲染相关操作requestAnimationFrame

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -45,5 +45,43 @@ function closeWindow() {
     log("winHandler.closed =", winHandler.closed)
 }
 
+let enabled = true
+
+function expensiveOperation() {
+    log("Invoke at", dateFormat("yyyy-MM-dd hh:mm:ss.nnn", new Date()))
+}
+
+window.addEventListener("scroll", () => {
+    if (enabled) {
+        enabled = !enabled;
+        // 请求浏览器开启动画帧绘制优化处理模式
+        let handle = requestAnimationFrame(expensiveOperation);
+        // 停止动画帧处理
+        // cancelAnimationFrame(handle)
+        setTimeout(() => enabled = true, 50)
+    }
+})
+
+function dateFormat(fmt, date) {
+    let ret;
+    const opt = {
+        "y+": date.getFullYear().toString(),        // 年
+        "M+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "h+": date.getHours().toString(),           // 时
+        "m+": date.getMinutes().toString(),         // 分
+        "s+": date.getSeconds().toString(),          // 秒
+        "n+": date.getMilliseconds().toString()
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
 
 
