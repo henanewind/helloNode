badc68382e500bea99e2fc50ea31c7952efec277
henanewind <zzwonder2009@126.com>
2021-05-05 16:34:47 +0800

    * 修改专用Worker工作者线程代码

--- src/main/js/worker.js
+++ src/main/js/worker.js
@@ -1,5 +1,8 @@
 console.log('加载专用工作者线程模块成功.....')
 // 向工作者线程发消息
 self.postMessage('foo')
-
-setInterval(()=>self.postMessage('bar..'), 100)
+// let count = 0;
+// setInterval(() => {
+//     count = count + 1;
+//     self.postMessage('bar.. this.count = ' + count)
+// }, 1000)
